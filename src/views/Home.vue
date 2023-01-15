5e1d0788681d463800e9090df513ffe4fb64f2ad
Nirlvy <1240629470@qq.com>
2023-01-15 21:26:48 +0800

    Add: card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,113 @@
 <template>
+  <div class="cardline">
+    <el-row :gutter="20">
+      <el-col :span="4">
+        <el-card :body-style="{ padding: '0px' }">
+          <img
+            src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+            class="image"
+          />
+          <div style="padding: 14px; display: block; text-align: center">
+            <span>总冰柜</span>
+            <div class="bottom">
+              <span class="value">{{ value.totalfreezer }}</span>
+            </div>
+          </div>
+        </el-card></el-col
+      >
+      <el-col :span="4">
+        <el-card :body-style="{ padding: '0px' }">
+          <img
+            src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+            class="image"
+          />
+          <div style="padding: 14px; display: block; text-align: center">
+            <span>运行冰柜</span>
+            <div class="bottom">
+              <span class="value">{{ value.totalfreezer }}</span>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="4">
+        <el-card :body-style="{ padding: '0px' }">
+          <img
+            src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+            class="image"
+          />
+          <div style="padding: 14px; display: block; text-align: center">
+            <span>需要补充</span>
+            <div class="bottom">
+              <span class="value">{{ value.totalfreezer }}</span>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="4">
+        <el-card :body-style="{ padding: '0px' }">
+          <img
+            src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+            class="image"
+          />
+          <div style="padding: 14px; display: block; text-align: center">
+            <span>总上架</span>
+            <div class="bottom">
+              <span class="value">{{ value.totalfreezer }}</span>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :span="4">
+        <el-card :body-style="{ padding: '0px' }">
+          <img
+            src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+            class="image"
+          />
+          <div style="padding: 14px; display: block; text-align: center">
+            <span>已售出</span>
+            <div class="bottom">
+              <span class="value">{{ value.totalfreezer }}</span>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
   <div id="main" class="main" style="width: 400px; height: 300px" />
 </template>
 
 <script lang="ts" setup>
 import * as echarts from "echarts"
-import { onMounted } from "vue"
+import { onMounted, reactive } from "vue"
+import request from "../utils/request"
 
 onMounted(() => {
-  echartsInit()
+  echartsInit
+  valueInit
+})
+
+interface ServerResponse {
+  data: ServerData
+}
+
+interface EchartsData {
+  x: string[]
+  y: number[]
+}
+
+interface ServerData {
+  code: string
+  data: EchartsData
+  msg: string
+}
+
+const value = reactive({
+  totalfreezer: 0,
+  runfreezer: 0,
+  stopfreezer: 0,
+  totalshelves: 0,
+  totalsold: 0,
+  waiting: 0,
 })
 
 const echartsInit = () => {
@@ -28,9 +128,34 @@ const echartsInit = () => {
       },
     ],
   }
+  request.get<ServerResponse, ServerData>("/echarts/example").then((res) => {
+    option.xAxis.data = res.data.x
+    option.series[0].data = res.data.y
+  })
   option && myChart.setOption(option)
-  console.log(1)
+}
+
+const valueInit = () => {
+  request.post("/")
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.cardline {
+  padding: 50px;
+}
+.value {
+  font-size: 12px;
+  color: #999;
+}
+
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
+
+.image {
+  width: 100%;
+  display: block;
+}
+</style>
